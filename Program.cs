a026e588a123ddcbd354ba6a2f0290ff23607b97
Simon Nilsson <s.i.m.o.n89.sn@gmail.com>
2026-02-22 11:55:03 +0100

    1st commit.

--- Program.cs
+++ Program.cs
@@ -1,34 +1,65 @@
+using UniversityCourses.Application.Dtos;
+using UniversityCourses.Application.Services;
+using UniversityCourses.Domain.Interfaces;
+using UniversityCourses.Infrastructure.Data;
+using UniversityCourses.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ValfriDbConnectionString")));
+
+
+//Services
+builder.Services.AddScoped<CourseService>();
+
+//Repos
+builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+
+
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddValidation();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+
+
+#region Courses
+var courses = app.MapGroup("/api/courses");
+
+courses.MapPost("/", async (CreateCourseDto dto, CourseService courseService) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    var result = await courseService.CreateCourseAsync(dto);
+
+    return result.IsSucess
+        ? Results.Created("", result.Data)
+        : Results.Conflict(result.Message);
 });
+
+courses.MapGet("/", async (CourseService courseService) =>
+{
+    var result = await courseService.GetAllCoursesAsync();
+    return Results.Ok(result.Data);
+});
+#endregion
+
+
+
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
